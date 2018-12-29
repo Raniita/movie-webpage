fa30c85cb04ba446cfb2ba321fa0dab708e272bf
Ranii <efdezsanchez@gmail.com>
2018-12-29 23:51:56 +0100

    Login KO. Debug 2

--- login.php
+++ login.php
@@ -71,9 +71,9 @@
     <img class="mb-4" src="img/tuxflix_logo.svg" alt="" width="220" height="220">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in!</h1>
     <label for="inputName" class="sr-only">Username</label>
-    <input type="text" id="inputName" class="form-control" placeholder="Username" required autofocus>
+    <input type="text" name="inputName" id="inputName" class="form-control" placeholder="Username" required autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <input type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password" required>
     <div class="checkbox mb-3">
         <label>
             <input type="checkbox" value="remember-me"> Remember me
